159d433dcbe002d5d89888cc18a9a80411ca2801
Dralix3 <53335leo@universidaddeleon.edu.mx>
2026-03-11 18:26:32 -0600

    guardar

--- Sucursal/urls.py
+++ Sucursal/urls.py
@@ -1,14 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
-from . import views
-
-urlpatterns = [
-    path('', views.Sucursal, name='Sucursal'),
-    path('sumar_sucursal/', views.sumar_sucursal, name='sumar_sucursal'),
-    path('eliminar_sucursal/<int:id>/', views.eliminar_sucursal, name='eliminar_sucursal'),
-    path('editar_sucursal/<int:id>/', views.editar_sucursal, name='editar_sucursal'),
-    path('actualizar_sucursal/<int:id>/', views.actualizar_sucursal, name='actualizar_sucursal'),
-=======
 from .views import pageSucursal,crearSucursal, editarSucursal, eliminarSucursal
 
 urlpatterns = [
@@ -21,5 +11,4 @@ urlpatterns = [
 
     path("eliminarSucursal/<int:id>/",eliminarSucursal),
 
->>>>>>> bab294d71edd8fa96fbef62573f93678978f6ea2
 ]
--- Sucursal/views.py
+++ Sucursal/views.py
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-from django.shortcuts import render, redirect
-from .models import Sucursal_pxn
-
-def Sucursal(request):
-    sucursales = Sucursal_pxn.objects.all()
-    return render(request, "Sucursal.html", {"sucursales": sucursales})
-
-def sumar_sucursal(request):
-    if request.method == "POST":
-        sucursal = Sucursal_pxn(
-            nombre_suc=request.POST['nombre_suc'],
-            direccion_suc=request.POST['direccion_suc'],
-            ciudad_suc=request.POST['ciudad_suc'],
-            telefono_suc=request.POST.get('telefono_suc', ''),
-            encargado_suc=request.POST.get('encargado_suc', ''),
-            email_suc=request.POST.get('email_suc', ''),
-            horario_suc=request.POST.get('horario_suc', ''),
-            estado_suc=request.POST.get('estado_suc', 'Activo'),
-            empleados_suc=request.POST.get('empleados_suc', 0)
-        )
-        sucursal.save()
-    return redirect('/pageSucursal/')
-
-def eliminar_sucursal(request, id):
-    sucursal = Sucursal_pxn.objects.get(id=id)
-    sucursal.delete()
-    return redirect('/pageSucursal/')
-
-def editar_sucursal(request, id):
-    sucursal = Sucursal_pxn.objects.get(id=id)
-    return render(request, "Sucursal.html", {"sucursal": sucursal})
-
-def actualizar_sucursal(request, id):
-    sucursal = Sucursal_pxn.objects.get(id=id)
-    if request.method == "POST":
-        sucursal.nombre_suc = request.POST['nombre_suc']
-        sucursal.direccion_suc = request.POST['direccion_suc']
-        sucursal.ciudad_suc = request.POST['ciudad_suc']
-        sucursal.telefono_suc = request.POST.get('telefono_suc', '')
-        sucursal.encargado_suc = request.POST.get('encargado_suc', '')
-        sucursal.email_suc = request.POST.get('email_suc', '')
-        sucursal.horario_suc = request.POST.get('horario_suc', '')
-        sucursal.estado_suc = request.POST.get('estado_suc', 'Activo')
-        sucursal.empleados_suc = request.POST.get('empleados_suc', 0)
-        sucursal.save()
-    return redirect('/pageSucursal/')
-=======
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Sucursal
 
@@ -114,4 +66,3 @@ def eliminarSucursal(request, id):
     sucursal.delete()
 
     return redirect("/pageSucursal/")
->>>>>>> bab294d71edd8fa96fbef62573f93678978f6ea2
